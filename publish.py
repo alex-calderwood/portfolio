89d8dde699f56edec5f64a696a25a2f1ba312fa5
Alex Calderwood <alexc@production.pro>
2019-07-11 16:07:29 -0400

    Made publish py3 compatible.

--- publish.py
+++ publish.py
@@ -9,14 +9,13 @@ def publish(dir, category):
         post = Post(os.path.join(dir, file), category=category)
 
         if db.session.query(Post.id).filter(Post.name == post.name).count() > 0:
-            print 'Post skipped, a post with this name already exists', post
+            print('Post skipped, a post with this name already exists', post)
         else:
-            print 'created', post
+            print('created', post)
             db.session.add(post)
 
     db.session.commit()
-    print
-    print "Created and published all posts in " + dir
+    print("Created and published all posts in " + dir)
 
 
 if __name__ == '__main__':
